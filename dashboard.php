afd4f3a296ad5e4c4da2e7bfa5edc8aa8a563104
marioashok93 <marioashok1@yahoo.com>
2018-05-07 18:44:02 +0530

    Added Bootstap css and javascript classes

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+
+	<title>Document</title>
+</head>
+<body>
+
+<p>Let's start</p>
+ 
+
+
+
+
+<script src="assets/js/jquery.js"></script>
+
+
+<script src="assets/js/bootstrap.min.js"></script>	
+</body>
+</html>
